84fba882bbdc31350e665f3b099a85b089fd748a
ertegun <ertegun@users.noreply.github.com>
2022-08-29 09:19:07 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
